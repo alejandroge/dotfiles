55f4c8ee313870b24721682a43b3920119a1c670
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2018-11-21 00:43:15 -0600

    Customize what gets install in my system

--- src/os/install/macos/main.sh
+++ src/os/install/macos/main.sh
@@ -11,16 +11,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 ./bash.sh
 ./../nvm.sh
 
-./browsers.sh
+#./browsers.sh
 ./compression_tools.sh
 ./git.sh
 ./gpg.sh
-./image_tools.sh
-./misc.sh
+#./image_tools.sh
+#./misc.sh
 ./misc_tools.sh
 ./../npm.sh
 ./tmux.sh
-./video_tools.sh
+#./video_tools.sh
 ./../vim.sh
 ./web_font_tools.sh
 
--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -12,12 +12,12 @@ upgrade
 ./build-essentials.sh
 ./../nvm.sh
 
-./browsers.sh
+#./browsers.sh
 ./compression_tools.sh
 ./git.sh
-./image_tools.sh
-./misc.sh
-./misc_tools.sh
+#./image_tools.sh
+#./misc.sh
+#./misc_tools.sh
 ./../npm.sh
 ./tmux.sh
 ./../vim.sh
